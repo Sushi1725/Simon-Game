1bf1498bd433bc6b3042b6a05609e24ad35c99ac
Sushi1725 <86946104+Sushi1725@users.noreply.github.com>
2023-04-14 18:19:36 +1000

    Using csv instead
    Used csv files instead of txt files. Now it can actually sort

--- score_sort_test.py
+++ score_sort_test.py
@@ -1,93 +1,121 @@
-lines = open('high_scores.txt', 'r').readlines()
-output = open("fasd.txt", 'w')
+# lines = open('high_scores.txt', 'r').readlines()
+# output = open("fasd.txt", 'w')
 
 
-# for line in sorted(lines, key=lambda line: line.split()[0], reverse=True):
-#     print(lines)
-#     placeholder = line.split()[0]
-#     print(placeholder)
-#     print(int(placeholder[0]))
-#     print(line.split())
-#     print(line)
-#     for sortScore in sorted(placeholder, key=lambda sortScore: int(sortScore[0]), reverse=True):
-#         # output.write(sortScore)
-#         print(sortScore)
-# e = []
-# for line in lines:
-#     linn = line.split(',')
-#     print(linn)
-#     lin = int(linn[0])
-#     print(lin)
-#     e.append(lin)
-#     print(e)
+# # for line in sorted(lines, key=lambda line: line.split()[0], reverse=True):
+# #     print(lines)
+# #     placeholder = line.split()[0]
+# #     print(placeholder)
+# #     print(int(placeholder[0]))
+# #     print(line.split())
+# #     print(line)
+# #     for sortScore in sorted(placeholder, key=lambda sortScore: int(sortScore[0]), reverse=True):
+# #         # output.write(sortScore)
+# #         print(sortScore)
+# # e = []
+# # for line in lines:
+# #     linn = line.split(',')
+# #     print(linn)
+# #     lin = int(linn[0])
+# #     print(lin)
+# #     e.append(lin)
+# #     print(e)
     
-# for l in sorted(e, reverse=True):
-#     print(l)
+# # for l in sorted(e, reverse=True):
+# #     print(l)
 
-# a = int(input())
-# if (a % 2) != 0:
-#     print('odd')
-# e = []
-# for line in lines:
-#     e.append(int(line.split(',')[0]) + line.split(',')[1])
-#     print(e)
+# # a = int(input())
+# # if (a % 2) != 0:
+# #     print('odd')
+# # e = []
+# # for line in lines:
+# #     e.append(int(line.split(',')[0]) + line.split(',')[1])
+# #     print(e)
     
-# for l in sorted(e, reverse=True):
-#     print(l)
+# # for l in sorted(e, reverse=True):
+# #     print(l)
+
+
+
+# # num = []
+# # e = []
+# # for line in lines:
+# #     e.append(line.split(',')[0])
+# #     e.append(line.split(',')[1])
+# #     print(e)
+    
+# # for index, l in enumerate(e):
+# #     try:
+# #         if(int(index) % 2) == 0:
+# #             # print('the index is odd (which is a number)')
+# #             num.append(int(l))
+# #             print(num)
+# #     except ValueError:
+# #         pass
+# # for new in sorted(num, reverse=True):
+# #     print(new)
 
 
 
 # num = []
 # e = []
+# ori = []
+# currentIndex = 0
 # for line in lines:
+#     # print(line)
 #     e.append(line.split(',')[0])
 #     e.append(line.split(',')[1])
-#     print(e)
+# # print(e)
     
-# for index, l in enumerate(e):
+# for index, l, in enumerate(e):
+#     # print(index, l)
 #     try:
 #         if(int(index) % 2) == 0:
-#             # print('the index is odd (which is a number)')
 #             num.append(int(l))
-#             print(num)
+#             # num.append(index)
+#             # print(num)
+#             try:
+#                 test = e[index+1]
+#                 # print(test)
+#                 ori.append(l)
+#                 ori.append(test)
+#                 # print(ori)
+#             except IndexError:
+#                 pass
 #     except ValueError:
 #         pass
-# for new in sorted(num, reverse=True):
+#     new = sorted(num, reverse=True)
 #     print(new)
-
-
-
-num = []
-e = []
-ori = []
-currentIndex = 0
-for line in lines:
-    # print(line)
-    e.append(line.split(',')[0])
-    e.append(line.split(',')[1])
-# print(e)
-    
-for index, l, in enumerate(e):
-    # print(index, l)
-    try:
-        if(int(index) % 2) == 0:
-            num.append(int(l))
-            # num.append(index)
-            # print(num)
-            try:
-                test = e[index+1]
-                # print(test)
-                ori.append(l)
-                ori.append(test)
-                # print(ori)
-            except IndexError:
-                pass
-    except ValueError:
-        pass
-    new = sorted(num, reverse=True)
-    print(new)
-    currentIndex = index
+#     currentIndex = index
     
 
-output.close()
-# lines.close()
+# output.close()
+# # lines.close()
+
+
+# # importing pandas package
+# import pandas
+
+# # assign dataset
+# csvData = pandas.read_csv("fasd.csv")
+
+# # displaying unsorted data frame
+# print("\nBefore sorting:")
+# print(csvData)
+
+# # sort data frame
+# csvData.sort_values(csvData.columns[0], axis=0, ascending=[False], inplace=True)
+
+# # displaying sorted data frame
+# print("\nAfter sorting:")
+# print(csvData)
+
+
+import sys
+import csv
+
+reader = csv.reader(open("fasd.csv"), delimiter=",")
+
+for Score, Name in reader:
+    sortedList = sorted(reader, key=lambda row: int(row[0]), reverse=True)
+    print(sortedList)
